8d16c35d194902bb68b3d1960ee1ec3a497e4fe0
SweatyClutcher <74849607+SweatyClutcher@users.noreply.github.com>
2021-05-12 18:19:58 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,27 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "DATABASE";
+const accessCode3 = "FUNCTION";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+
+    fill("white")
+    textSize(15)
+    text("A T A D A S B A", 700,150)
+    fill("lightblue")
+    text("Hint: Place to store info", 700,170)
+
+    fill("white")
+    textSize(15)
+    text("C U T N I F O N", 200,200)
+    fill("lightblue")
+    text("Hint: Does a specific task", 200,220)
+// add code for displaying rest of the names and the hints.
+    
+
+}
